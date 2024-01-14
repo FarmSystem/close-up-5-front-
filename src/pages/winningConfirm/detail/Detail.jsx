d19b28122c6477582e15ad56c8b3feaa6c63b0fc
thisishwan2 <feel000617@gmail.com>
2024-01-14 14:37:35 +0900

    feat: raffle 당첨 확인 디테일 생성

--- src/pages/winningConfirm/detail/Detail.jsx
+++ src/pages/winningConfirm/detail/Detail.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
 import * as S from './style';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+// import Header from '../../../components/raffleApplyment/header/Header';
+import RaffleConfirmDetail from '../../../components/raffleConfirmDetail/Detail';
+import ProfileImg from '../../../assets/images/raffleApplyment/profile.png';
+
+// api
+import axios from '../../../api/axios';
 
 function Detail() {
-  return <S.DetailWrapper>Detail 페이지</S.DetailWrapper>;
+  const { id } = useParams();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`/user/raffles/${id}`);
+      setData(response.data.result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <>
+      {data && (
+        <RaffleConfirmDetail
+          winningInfo={data.winningInfo} //
+          raffleCreateDate={data.raffleCreateDate}
+          endDraffleProductStartDateate={data.raffleProductStartDate}
+          raffleProductEndDate={data.raffleProductEndDate}
+          raffleProductTitle={data.raffleProductTitle}
+          raffleProductThumbnailUrl={data.raffleProductThumbnailUrl}
+          raffleProductContent={data.raffleProductContent}
+          creatorName={data.creatorName}
+          winningProductUrl={data.winningProductUrl}
+          raffleWinningDate={data.raffleWinningDate}
+        />
+      )}
+    </>
+  );
 }
 
 export default Detail;
