82ec88af85f2827423020b5fe4848e5e262e3386
anne <wwhurin0515@gmail.com>
2024-01-15 03:46:17 +0900

    Feat: 래플 작성 완료 페이지 (임시)

--- src/pages/raffleWriting/complete/Complete.jsx
+++ src/pages/raffleWriting/complete/Complete.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Header from '../../../components/raffleWriting/header/Header';
 import * as S from './style';
 
 function Complete() {
-  return <S.CompleteWrapper>Complete 페이지</S.CompleteWrapper>;
+  const location = useLocation();
+  return (
+    <>
+       <Header title={'래플 폼 작성'} />
+       <Header title={'응모일은'} />
+       <Header title={location.state} />
+       <Header title={'입니다.'} />
+       <Header title={'래플 작성이 완료되었습니다.'} />
+    </>
+  )
 }
 
 export default Complete;
