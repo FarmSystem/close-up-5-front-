4f1eb12b8abb2de689d565d1854681679f65ebeb
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2024-01-16 00:25:33 +0900

    Feat: 전체 래플 목록 페이지 구현

--- src/pages/raffle/box/Box.jsx
+++ src/pages/raffle/box/Box.jsx
@@ -1,8 +1,61 @@
-import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect } from 'react';
 import * as S from './style';
+import axios from '../../../api/axios';
 
 function Box() {
-  return <S.BoxWrapper>래플 박스 페이지</S.BoxWrapper>;
+  const [raffleProducts, setRaffleProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          '/user/raffle-products?page=1&size=10'
+        );
+        console.log(response.data.result.content);
+        setRaffleProducts(response.data.result.content);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false); // Set loading to false when data fetching is complete
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <S.BoxWrapper>
+      <S.RaffleTitle>래플 목록</S.RaffleTitle>
+      <S.RaffleView>
+        <S.RaffleViewTitle>전체 래플 목록</S.RaffleViewTitle>
+        <S.RaffleLine> </S.RaffleLine>
+        {loading ? (
+          <p>Loading…</p>
+        ) : (
+          raffleProducts.map(product => (
+            // eslint-disable-next-line react/jsx-key
+            <S.StyledLink to={`/raffle/${product.raffleProductId}`}>
+              <S.RaffleContents key={product.raffleProductId}>
+                <S.RaffleContentsImages src={product.raffleProductThumbnail} />
+                <S.RaffleContentslist>
+                  <S.RaffleContentsTitle>
+                    {product.raffleProductTitle}
+                  </S.RaffleContentsTitle>
+                  <S.RaffleContentsDate>
+                    {product.startDate} ~ {product.endDate}
+                  </S.RaffleContentsDate>
+                  <S.RaffleContentsPrice>
+                    {product.raffleProductPrice}Point
+                  </S.RaffleContentsPrice>
+                </S.RaffleContentslist>
+              </S.RaffleContents>
+            </S.StyledLink>
+          ))
+        )}
+      </S.RaffleView>
+    </S.BoxWrapper>
+  );
 }
 
 export default Box;
