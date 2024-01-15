08399550094e0f6a03711db3103f3a92de5da25a
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2024-01-16 00:28:37 +0900

    Feat: 전체 당첨 확인 페이지 구현

--- src/pages/winningConfirm/box/Box.jsx
+++ src/pages/winningConfirm/box/Box.jsx
@@ -1,8 +1,60 @@
-import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect } from 'react';
 import * as S from './style';
+import axios from '../../../api/axios';
 
 function Box() {
-  return <S.BoxWrapper>Box 페이지</S.BoxWrapper>;
+  const [raffleProducts, setRaffleProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/user/raffles/winning');
+        setRaffleProducts(response.data.result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false); // Set loading to false when data fetching is complete
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <S.BoxWrapper>
+      <S.RaffleTitle>당첨 확인</S.RaffleTitle>
+      <S.RaffleView>
+        <S.RaffleViewTitle>전체 당첨 목록</S.RaffleViewTitle>
+        <S.RaffleLine> </S.RaffleLine>
+        {loading ? (
+          <p>Loading…</p>
+        ) : (
+          raffleProducts.map(product => (
+            <S.styledLink
+              to={`/winningConfirm/${product.raffleId}`}
+              key={product.raffleId}
+            >
+              <S.RaffleContents>
+                <S.RaffleContentsImages src={product.raffleThumbnailUrl} />
+                <S.RaffleContentslist>
+                  <S.RaffleContentsTitle>
+                    {product.raffleTitle}
+                  </S.RaffleContentsTitle>
+                  <S.RaffleContentsDate>
+                    {product.raffleCreatedAt} ~ {product.raffleEndAt}
+                  </S.RaffleContentsDate>
+                  <S.RaffleContentsPrice>
+                    {product.winningInfo}Point
+                  </S.RaffleContentsPrice>
+                </S.RaffleContentslist>
+              </S.RaffleContents>
+            </S.styledLink>
+          ))
+        )}
+      </S.RaffleView>
+    </S.BoxWrapper>
+  );
 }
 
 export default Box;
