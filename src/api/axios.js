a47107dcb5cc694d989645b414b2c681cf38f2c0
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2024-01-16 21:57:07 +0900

    Remove: 콘솔 창 토큰 출력 삭제

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const getAccessToken = () => {
   return localStorage.getItem('accessToken');
 };
-console.log(getAccessToken());
+
 const instance = axios.create({
   baseURL: 'http://localhost:8080/',
   withCredentials: true,
