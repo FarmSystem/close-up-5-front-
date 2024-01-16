d8bbe33b14ed600eb5186d8f4655a143ae83cf8c
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2024-01-17 01:17:39 +0900

    Design: 로그인 페이지 디자인

--- src/pages/logIn/LogIn.jsx
+++ src/pages/logIn/LogIn.jsx
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation, Link } from 'react-router-dom';
+import React from 'react';
 import * as S from './style';
-import womanImage from '../../../src/assets/images/logo/woman.png';
-import backImage from '../../../src/assets/images/back/return.png';
-import axios from '../../../src/api/axios';
-
+import womanImage from '../../../src/assets/images/login/woman.png';
+import KaKaoButton from '../../assets/images/login/kakao.png';
 
 function LogIn() {
   const handleButtonClick = () => {
@@ -13,23 +10,20 @@ function LogIn() {
   };
 
   return (
-    <S.LogInWrapper style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div style={{ width: 200, height: 9, left: 80, top: -100, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 30, fontFamily: 'Karla', fontWeight: '700', lineHeight: 20, wordWrap: 'break-word' }}>CLOSEUP</div>
-      <div style={{ width: 283, height: 540.24, position: 'relative' }}>
+    <S.LogInWrapper>
+      <S.LogInTitle>CLOSEUP</S.LogInTitle>
 
-        {/* 사진과 서비스 소개 */}
-        <div style={{ width: 282, height: 291, left: 0, top: 34, position: 'absolute', background: `url(${womanImage})`, backgroundSize: 'cover', borderRadius: 40 }}></div>
-        <div style={{ width: 280, height: 33, left: 3, top: 369, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Noto Sans KR', fontWeight: '700', wordWrap: 'break-word' }}>크리에이터 삶을 더 가까이</div>
-        <div style={{ width: 276, height: 33, left: 2, top: 411, position: 'absolute', textAlign: 'center', color: 'rgba(255, 255, 255, 0.70)', fontSize: 14, fontFamily: 'Noto Sans KR', fontWeight: '350', wordWrap: 'break-word' }}>다양한 래플 서비스를 통해<br />한정 크리에이터 서비스를 제공 </div>
-       
-        {/* 카카오톡으로 로그인 */}
-        <Link to="/" onClick={handleButtonClick}>
-      <div style={{ width: 279, height: 37.24, left: 3, top: 459, position: 'absolute', background: '#FFE522', borderRadius: 10 }}>
-        <div style={{ width: 279, height: 37.24, left: 0, top: 0, position: 'absolute', borderRadius: 25 }} />
-      </div>
-      <div style={{ width: 260, height: 16, left: 21, top: 472, position: 'absolute', textAlign: 'center', color: '#3D2225', fontSize: 16, fontFamily: 'Noto Sans KR', fontWeight: '700', wordWrap: 'break-word' }}>카카오톡으로 간편로그인</div>
-    </Link>
-      </div>
+      {/* 사진과 서비스 소개 */}
+      <S.LogInImg src={womanImage} />
+      <S.LogInContent> 크리에이터 삶을 더 가까이</S.LogInContent>
+      <S.LogInSubContent>
+        다양한 래플 서비스를 통해
+        <br />
+        한정 크리에이터 서비스를 제공
+      </S.LogInSubContent>
+
+      {/* 카카오톡으로 로그인 */}
+      <S.LogInKakaoButton src={KaKaoButton} onClick={handleButtonClick} />
     </S.LogInWrapper>
   );
 }
--- src/pages/logIn/style.jsx
+++ src/pages/logIn/style.jsx
@@ -1,5 +1,51 @@
 import { styled } from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const LogInWrapper = styled.div`
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const LogInTitle = styled.div`
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+  color: #fff;
+  text-align: center;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 20px;
+  margin-bottom: 15px;
+`;
+
+export const LogInImg = styled.img`
+  width: 282px;
+  height: 291px;
+`;
+
+export const LogInContent = styled.div`
+  color: #fff;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: normal;
+  margin-top: 44px;
+`;
+
+export const LogInSubContent = styled.div`
+  color: rgba(255, 255, 255, 0.7);
+  text-align: center;
+  font-size: 14px;
+  font-weight: 350;
+  line-height: normal;
+  margin-top: 10px;
+`;
+
+export const LogInKakaoButton = styled.img`
+  display: flex;
+  width: 279px;
+  height: 50px;
+  margin-top: 32px;
+  cursor: pointer;
 `;
