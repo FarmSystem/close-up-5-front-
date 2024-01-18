ba8ea20cbe802d936707459a03a18c98fd92c7fe
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2024-01-18 23:53:26 +0900

    Fix: 당첨 확인 목록 API 변경

--- src/pages/winningConfirm/box/Box.jsx
+++ src/pages/winningConfirm/box/Box.jsx
@@ -10,7 +10,7 @@ function Box() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('/user/raffles/winning');
+        const response = await axios.get('/user/raffles');
         setRaffleProducts(response.data.result);
       } catch (error) {
         console.error('Error fetching data:', error);
